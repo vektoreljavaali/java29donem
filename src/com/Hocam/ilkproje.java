3e572d628deb74856940bf75a1ebf3b0f5eb0a6f
vektoreljavaali <34411669+vektoreljavaali@users.noreply.github.com>
2017-12-11 14:23:03 +0300

    Ödev içerikleri
    yeni ödev içeriklerini tanımladım

--- src/com/Hocam/ilkproje.java
+++ src/com/Hocam/ilkproje.java
@@ -1,6 +1,27 @@
 package com.Hocam;
 
 public class ilkproje {
- 
-	int a = 10;
+ 	// YENİ ÖDEVLER 
+	
+	//KOLAY SORULAR
+	//01- 20 den başlayarak 10 a kadar olan sayıları ekrana yazdıran kod //for-while-do..while tüm döngü yapılarında çalıştırın
+	//02- Kulanıcıdan iki sayı girmesini bekleyen programın hangi sayının büyük olduğunu bulması bekleniyor. Yani a>b ise ekrana a sayısını yazdırınız.
+	//03- 5 ten başlayarak 3 er artarak 20 ye kadar olan sayıları yazdıran program.  5,8,11,14,17,20 yazacak.
+	//04- girilen sayının nefatif mi pozitif mi olduğunu bulan program.
+	//05- girilen 3 sayıdan hangisinin büyük olduğunu bulan kod.
+	
+	
+	//ZOR SORULAR
+	//01- 9.000 den başlayarak 10.000 e kadar olan sayıların içinden tek ve çift sayıları bulup toplamını ekrana yazdıran,
+	//02- 1 den 1.000 e kadar olan sayıların içindeki asal sayıları bulan program.
+	//03- öğrenciden vize ve final notlarını alan ve ortalama notu hesaplayıp. harf karışılığını bulan program. AA,AB.....FF 
+	//04- 3,1,33,12,6,90 sayılarını küçükten büyüğe sıralayınız.
+	//05- yıldız simgesi kullanılarak istenilen uzunlukta kare çiziniz. mesela: 5 yıldız olsun
+	//  * * * * * 
+	//  *       *
+	//  *       *
+	//  *       *
+	//  * * * * *
+
+	
 }

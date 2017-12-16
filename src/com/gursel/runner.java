09668fad17de14c0f0e3fcc218a9a0344a270e8b
user <user@user-Bilgisayar>
2017-12-16 19:36:58 +0200

    örnek kod

--- src/com/gursel/runner.java
+++ src/com/gursel/runner.java
@@ -3,7 +3,25 @@ package com.gursel;
 public class runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	         
+		 /* çift ve tek sayýlarýn karelerinin toplamý*/
+		
+		int Csonuc=0;
+		int Tsonuc=0;
+	  for(int i=1;i<=4;i++)
+	  {
+		  if(i%2==0)
+		  {
+			  Csonuc=Csonuc+(i*i);
+			  System.out.println("Çift syýlarýn karelerinin toplamý : "+ Csonuc);
+		  }
+		  else
+		  {
+			  Tsonuc=Tsonuc+(i*i);
+			  System.out.println("Tek syýlarýn karelerinin toplamý : "+ Tsonuc);
+		  }
+	  }
+	  System.out.println("Csonuc ve Tsonuc toplamý : "+ (Csonuc+Tsonuc));
 
 	}
 
